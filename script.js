d2c9929cee7c5baa86bdc03eed3189bb118435d5
Prakhar Patel <prakhar.codes433@gmail.com>
2025-12-17 07:39:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const characters = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+  '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|',
+  ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?',
+  '`', '~', ' ',
+];
+
+
+function generatePassword(length) {
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+  
+    const randomIndex = Math.floor(Math.random() * characters.length);
+   
+    result += characters[randomIndex];
+  }
+  return result;
+}
+
+function getPassword () {
+  let passcontainer = document.querySelector(".password-container");
+  passcontainer.style.visibility = "visible";
+  const myNewPassword = generatePassword(16);
+ let passfield1 = document.getElementById('pass-field-1');
+ passfield1.innerText = myNewPassword;
+ const myNewPassword2 = generatePassword(16);
+ let passfield2 = document.getElementById('pass-field-2');
+ passfield2.innerText = myNewPassword2;
+}
+
+
